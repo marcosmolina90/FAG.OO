8a39437aa21e030e88d3a28186949e8a98e419f2
Marcos Alexandre Molina <marcos.molina@intranet.maxiconsystems.com.br>
2022-04-19 22:02:43 -0300

    Update Main.java

--- FaculdadeBD/src/main/java/br/edu/fag/teste/Main.java
+++ FaculdadeBD/src/main/java/br/edu/fag/teste/Main.java
@@ -5,6 +5,8 @@ import br.edu.fag.modelo.Estado;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.swing.*;
+import java.util.List;
 
 public class Main {
     private static EntityManagerFactory entityManagerFactory =
@@ -15,21 +17,28 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("rodou");
-		
-		
-		// FIND
+
+ String opc =        JOptionPane.showInputDialog("1 - listar \n " +
+                        "2 - Inserir");
+		switch (opc){
+            case "1":
+                List<Estado> listEstado = listEstado();
+                for (Estado e: listEstado){
+                    JOptionPane.showMessageDialog(null,
+                                                     e.toString());
+                }
+                break;
+            case "2":
+                inserir();
+                break;
+        }
+
+        // FIND
 //		Estado estado = entityManager.find(Estado.class, 1);
 //		System.out.println("Nome do estado:" + estado.getNome());
 		
-		// INSERT
-		Estado estado = new Estado();
-        estado.setCodigo("41");
-        estado.setSigla("PR");
-        estado.setNome("Parana");
-//		
-		entityManager.getTransaction().begin();
-		entityManager.persist(estado);
-        entityManager.getTransaction().commit();
+
+
 		
 		// DELETE
 //		Estado estado = entityManager.find(Estado.class, 2);
@@ -48,5 +57,27 @@ public class Main {
 
 		entityManager.close();
 		entityManagerFactory.close();
+    }
+
+    private static void inserir() {
+        Estado estado = new Estado();
+        estado.setCodigo(JOptionPane.showInputDialog("Informe Código"));
+        estado.setSigla(JOptionPane.showInputDialog("Informe Sigla"));
+        estado.setNome(JOptionPane.showInputDialog("Informe Nome"));
+//
+        entityManager.getTransaction().begin();
+        entityManager.persist(estado);
+        entityManager.getTransaction().commit();
+
+    }
+
+    public static List<Estado> listEstado(){
+        entityManager.getTransaction().begin();
+        List<Estado> retorno =
+        entityManager.createNativeQuery("select * from Estado ",
+                                                           Estado.class)
+                .getResultList();
+        entityManager.close();
+        return retorno;
     }
 }
