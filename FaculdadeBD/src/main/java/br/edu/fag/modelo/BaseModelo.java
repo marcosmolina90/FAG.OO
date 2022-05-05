6af3cb85cdab78ba97b4a642cd41df937bfdeb91
Marcos Alexandre Molina <marcos.molina@intranet.maxiconsystems.com.br>
2022-05-04 21:55:20 -0300

    Update BaseModelo.java

--- FaculdadeBD/src/main/java/br/edu/fag/modelo/BaseModelo.java
+++ FaculdadeBD/src/main/java/br/edu/fag/modelo/BaseModelo.java
@@ -7,16 +7,42 @@ import java.util.UUID;
 @MappedSuperclass
 public abstract class BaseModelo {
 
-
     @Column
     @Temporal(TemporalType.TIMESTAMP)
     private Date dtCriacao;
+
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dtUpdate;
+
+    private Integer versao;
 
     @PrePersist
     private void prePersist(){
         if(this.dtCriacao == null){
             this.dtCriacao = new Date();
         }
+        if(this.versao == null){
+            this.versao = 0;
+        }else{
+            this.versao++;
+        }
+
+        this.dtUpdate = new Date();
+    }
+
+    @PreUpdate
+    private void preUpdate(){
+        if(this.dtCriacao == null){
+            this.dtCriacao = new Date();
+        }
+        if(this.versao == null){
+            this.versao = 0;
+        }else{
+            this.versao++;
+        }
+
+        this.dtUpdate = new Date();
     }
 
 
